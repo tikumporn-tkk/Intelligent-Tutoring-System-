12ae4a9811904456c528dad5cbbefc88872ab444
tikumporn khamkhong <71635427+tikumporn-tkk@users.noreply.github.com>
2021-11-13 21:06:43 +0700

    Delete command_hint1.php

--- command_hint1.php
+++
@@ -1,7 +0,0 @@
-<?php
- $score_command =  $_SESSION['score_command'];
- echo "<script language=\"JavaScript\">";
- echo "alert('ต้องใช้ตัวช่วยที่ 1 โดยจะมีการหักคะแนน 25 %');";	
- echo "</script>";
-
-?>
