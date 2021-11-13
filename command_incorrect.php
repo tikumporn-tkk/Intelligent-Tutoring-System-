2731330609f65ad4b044684ddffd8d33181d0a96
tikumporn khamkhong <71635427+tikumporn-tkk@users.noreply.github.com>
2021-11-13 21:07:33 +0700

    Delete command_incorrect.php

--- command_incorrect.php
+++
@@ -1,6 +0,0 @@
-<?php
- $score_command =  $_SESSION['score_command'];
- echo "<script language=\"JavaScript\">";
- echo "alert('ไม่สามารถแสดงผลลัพธ์ได้ โปรดลองใหม่อีกครั้ง');";	
- echo "</script>";
-?>
