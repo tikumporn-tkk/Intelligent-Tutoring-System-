349c9b17a9126de02bf481ef2966545e4a4a62aa
tikumporn khamkhong <71635427+tikumporn-tkk@users.noreply.github.com>
2021-11-13 21:06:05 +0700

    Delete code1.c

--- code1.c
+++
@@ -1,5 +0,0 @@
-#include<stdio.h>
-                                                  void main() {
-                                                  printf ("Hello World2!\n");
-                                                  printf ("This is my GALAX C");
-                                                  }
