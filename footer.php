05471bf8b1ee791a9fc6700fa8d350e247fcb163
tikumporn khamkhong <71635427+tikumporn-tkk@users.noreply.github.com>
2021-11-13 21:08:29 +0700

    Delete footer.php

--- footer.php
+++
@@ -1,4 +0,0 @@
-   <div class="pull-right hidden-xs">
-   <strong>BY Tikumporn Khamkhong & Panamkorn Promkeeree</strong>
-    </div>
-    <strong>Copyright &copy;2019 <a href="#">Web-based Intelligent Tutoring System for C Programming.</a>.</strong>
