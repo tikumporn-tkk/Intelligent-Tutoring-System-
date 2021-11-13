cce263fcf5bde5398fc313d97654558b3cf1631a
tikumporn khamkhong <71635427+tikumporn-tkk@users.noreply.github.com>
2021-11-13 21:05:28 +0700

    Delete choice_incorrect.php

--- choice_incorrect.php
+++
@@ -1,6 +0,0 @@
-<?php
- $score_command =  $_SESSION['score_command'];
- echo "<script language=\"JavaScript\">";
- echo "alert('ได้คะแนนข้อนี้ $score_command คะแนน กรุณณากดข้อถัดไป');";	
- echo "</script>";
-?>
